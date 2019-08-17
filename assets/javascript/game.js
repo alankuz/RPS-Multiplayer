833b668b0cf35a036c0d74445801f79662b3a262
Alan Kuzmanovic <alankuzmanovic@gmail.com>
2019-08-16 23:11:51 -0600

    game still working. Players wont change to player 1 and the chat is not working but is recording. Needs some work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,10 @@ var playernum = 2
 var playercount = 1
 var isready = 0
 var pname = prompt("Enter Name")
-var rdypress = 0
+var rdypress = 0;
 var isplayer1 = true;
-var assigned= false;
+var assigned = false;
+
 
 
 var firebaseConfig = {
@@ -20,6 +21,11 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 var database = firebase.database()
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected");
+var isonline = database.ref('/isonline')
+database.ref('/isonline').set('false')
+var onlinecheck;
 
 
 $(document).on('click', '#rock', function () {
@@ -27,12 +33,14 @@ $(document).on('click', '#rock', function () {
     choice = 0
     console.log(choice)
     console.log(playercount)
+    $('#winlose').html('')
     readybtn()
 })
 $(document).on('click', '#paper', function () {
 
     choice = 1
     console.log(choice)
+    $('#winlose').html('')
     readybtn()
 
 })
@@ -40,6 +48,7 @@ $(document).on('click', '#scissors', function () {
 
     choice = 2
     console.log(choice)
+    $('#winlose').html('')
     readybtn()
 
 })
@@ -48,29 +57,57 @@ function readybtn() {
     // todo grey out the other buttons so you cant change your choice. make sure to add a way out on reset. 
 }
 database.ref().on("value", function (snapshot) {
+    onlinecheck = snapshot.child('/isonline').val()
+    var online = database.ref('/isonline');
     console.log(snapshot.child('user/p1choice').val())
     if (assigned === false) {
         if (snapshot.hasChild("user/p1playercount")) {
             playercount = 2
             isplayer1 = false;
             console.log('assigned player2--' + playercount)
-            assigned=true;
+            assigned = true;
         } else {
             playercount = 1
             console.log('assigned player1---' + playercount)
             isplayer1 = true;
-            assigned=true;
+            assigned = true;
+
         }
     }
 
     if ((snapshot.hasChild("user/p1playercount")) && snapshot.hasChild("user2/p2playercount")) {
-        gamestart(snapshot);
+        database.ref('/isonline').set('true')
+        var ready1 = snapshot.child('user/p1ready').val()
+        var ready2 = snapshot.child('user2/p2ready').val()
+        if (ready1 === 1 && ready2 === 1) {
+            gamestart(snapshot);
+        }
     }
-
+    online.onDisconnect().set('false')
 })
 $(document).on('click', '#ready', function () {
-
+    console.log(rdypress)
     var isready = 1;
+    if (rdypress === 1) {
+        console.log('anschange-----')
+        isready = 1
+        if (playercount === 1) {
+            database.ref('/user').set({
+                p1name: pname,
+                p1choice: choice,
+                p1ready: isready,
+                p1playercount: playercount
+            })
+        }
+        if (playercount === 2) {
+            database.ref('/user2').set({
+                p2name: pname,
+                p2choice: choice,
+                p2ready: isready,
+                p2playercount: playercount
+            })
+        }
+    }
     if (rdypress === 0) {
         if (isplayer1 === true) {
             database.ref('/user').set({
@@ -170,4 +207,48 @@ function gamestart(snapshot) {
             }
         }
     }
+    isready = 0
+    if (playercount === 1) {
+        database.ref('/user').set({
+            p1name: pname,
+            p1choice: choice,
+            p1ready: isready,
+            p1playercount: playercount
+        })
+    }
+    if (playercount === 2) {
+        database.ref('/user2').set({
+            p2name: pname,
+            p2choice: choice,
+            p2ready: isready,
+            p2playercount: playercount
+        })
+    }
+    $('#readybtn').html('')
 }
+$(document).on('click', '#chatsubmit', function() {
+database.ref('chatmessage').push(name + ": " + $('#chat').val())
+// var myRef = firebase.database().ref.push().key
+// var key = myRef.key;
+// console.log(key)
+
+})
+// database.ref().on("value", function (snapshot) {
+// $('#chatroom').text(snapshot.child)
+// })
+connectedRef.on("value", function (snap) {
+    var p1stuff = database.ref('/user');
+    var p2stuff = database.ref('/user2');
+
+    // If they are connected..
+    if (snap.val()) {
+
+        // Add user to the connections list.
+        //   console.log(snap.child('.info/connected').val())
+
+        // Remove user from the connection list when they disconnect.
+        p1stuff.onDisconnect().remove();
+        p2stuff.onDisconnect().remove();
+
+    }
+});
